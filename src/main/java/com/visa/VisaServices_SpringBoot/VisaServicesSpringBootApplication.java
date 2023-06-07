69a0f7643bb1877e71c6c36887ef2c4a06d56c93
Riyaz <riajmazumder711@gmail.ocm>
2023-06-07 18:41:39 +0600

    initial adding

---
+++ src/main/java/com/visa/VisaServices_SpringBoot/VisaServicesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.visa.VisaServices_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaServicesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaServicesSpringBootApplication.class, args);
+	}
+
+}
