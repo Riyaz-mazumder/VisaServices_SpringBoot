71115d7f9beca138caf2c5b726333f6d058880c2
Riyaz <riajmazumder711@gmail.ocm>
2023-06-11 18:11:04 +0600

    adding

--- src/main/java/com/visa/VisaServices_SpringBoot/repository/VisaRepo.java
+++ src/main/java/com/visa/VisaServices_SpringBoot/repository/VisaRepo.java
@@ -3,8 +3,14 @@ package com.visa.VisaServices_SpringBoot.repository;
 
 import com.visa.VisaServices_SpringBoot.models.VisaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VisaRepo extends JpaRepository<VisaModel, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM VisaModel where sub_cate = ?")
+    public VisaModel searchVisa(String subCategory);
 }
